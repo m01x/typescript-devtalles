a692f080c50f506dd9bdcf0ef62ba2b5eb4e5916
m01x <fortiz.informatica@gmail.com>
2025-01-16 19:30:22 -0300

    Carga base de archivos, inicio del curso.

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = 'hola mundo!!!';
+const hero = {
+    name: 'Ironman',
+    age: 45
+};
+hero.age = 40;
+console.log(hero);
---
+++ bases/app.ts
@@ -0,0 +1,10 @@
+const msg: string = 'hola mundo!!!';
+
+const hero = {
+    name: 'Ironman',
+    age: 45
+}
+
+hero.age = 40;
+
+console.log(hero);
